cbda5dbb92e21897aaa179f124cd58fe94048d17
PauloAlves8039 <pj38alves@gmail.com>
2020-11-24 21:07:48 -0300

    Adicionando a action Post

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using src.Api.Domain.Entities;
 using src.Api.Domain.Interfaces.Services.User;
 
 namespace src.Api.Application.Controllers
@@ -60,6 +61,32 @@ namespace src.Api.Application.Controllers
             try
             {
                 return Ok(await _service.Get(id));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] UserEntity user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Post(user);
+                if (result != null)
+                {
+                    return Created(new Uri(Url.Link("GetWithId", new { id = result.Id })), result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
             }
             catch (ArgumentException e)
             {
