a25ac311e7e09710a68fd8b6477164a967ab589e
PauloAlves8039 <pj38alves@gmail.com>
2020-12-11 23:56:22 -0300

    Implementação do LoginService

---
+++ src/Api.Domain/Interfaces/Services/User/ILoginService.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using src.Api.Domain.Entities;
+
+namespace src.Api.Domain.Interfaces.Services.User
+{
+    public interface ILoginService
+    {
+        /// <summary>
+        /// Método responsável pela pesquisa de login do usuário.
+        /// </summary>
+        /// <param name="user">Parâmetro de pesquisa.</param>
+        /// <returns>Retorna uma Task com o login do usuário pesquisado.</returns>
+        Task<object> FindByLogin(UserEntity user);
+    }
+}
